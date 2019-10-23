9ad65be0096daee77efbcf4f2222705f27f11709
Simple-Shyam <shyam055119@gmail.com>
2019-10-23 18:21:00 +0800

    Macro to read energy loss and MC Points

--- start/check.C
+++ start/check.C
@@ -1,4 +1,4 @@
-///////////// code for checking the basic things of particles by shaym kumar
+///////////// code for checking the basic things works in ALICE and also in PANDA of particles by shyam kumar
 
 #include "TGraphErrors.h"
 #include "TF1.h"
@@ -10,10 +10,6 @@
 void check()
    {
 
-
-////////////////loading basic library
-gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
-    rootlogon();
 ////////////////////////////////////////////////////// style of the plot
    gStyle->SetPalette(1);
    gStyle->SetOptTitle(0);
@@ -22,39 +18,34 @@ gROOT->LoadMacro("$VMCWORKDIR/gconfig/rootlogon.C");
    gStyle->SetLabelSize(.05,"X");gStyle->SetLabelSize(.05,"Y");
    gStyle->SetHistLineWidth(2);
    gStyle->SetOptFit(1);
+     
 ///////////////////////////////////////////////Reading the root file
- TFile *f = TFile::Open("sim_complete.root");
- TTree *cbmsim = (TTree*)f->Get("cbmsim"); 
+ TFile *f = TFile::Open("test.root");
+ TTree *sim = (TTree*)f->Get("cbmsim"); 
   
 TCanvas* c1 = new TCanvas("Simple plot", "Simulation", 100, 100, 800, 800);
 c1->Divide(2,2);
-////////////////////////////////////////////////////
+TH1D *Pdg = new TH1D("Pdg","Pdg code of Particles",100,0,2300);
+TH2D *Points= new TH2D("Points","MC Points",100,-50,50,100,-50,50);
+TH1D *Zpoints = new TH1D("Zpoints","Z Points on TPC",100,0,2300);
+TH1D *Eloss = new TH1D("Eloss","Energy loss for each MC Point",100,0,20);
 
-//while(cbmsim->("MCTrack.GetPoints()")! =6008855);
-//{
-
-//for(Int_t i= 0; i < cbmsim->("MCTrack.GetPoints()") ; i++)
-
-//if(cbmsim->("MCTrack.GetPdgCode()")==211)
-//{
 //////////////////////////////////////////////////////////////
 c1->cd(1);
-cbmsim->Draw("MCTrack.GetPdgCode()>> Pdg(100,0,2300)");
+sim->Draw("MCTrack.fPdgCode>> Pdg");
 Pdg->GetXaxis()->SetTitle("MCTrack.Pdgcode");
 //////////////////////////////////////////////////////////////////////
 c1->cd(2);
-cbmsim->Draw("STARTPoint.GetX():STARTPoint.GetY()>> Points(100,-14,14,100,-14,14)","","colz");
-Points->GetXaxis()->SetTitle("X hits");
-Points->GetYaxis()->SetTitle("Y hits");
+sim->Draw("CeeTpcPoint.fX:CeeTpcPoint.fY>> Points","","colz");
+Points->GetXaxis()->SetTitle("X Points");
+Points->GetYaxis()->SetTitle("Y Points");
 /////////////////////////////////////////////////////////////////////
 c1->cd(3);
-cbmsim->Draw("STARTPoint.GetZ()>> Zpoints(100,38,41)");
+sim->Draw("CeeTpcPoint.fZ>> Zpoints");
 Zpoints->GetXaxis()->SetTitle("Z Points");
 ///////////////////////////////////////////////////////////////////////////////////
 c1->cd(4);
-cbmsim->Draw("(STARTPoint.GetEnergyLoss())*1000000>> Eloss(100,0,400)");
+sim->Draw("CeeTpcPoint.fELoss*1000000 >> Eloss");
 Eloss->GetXaxis()->SetTitle("Energy Loss (KeV)");
 
-//else
-//exit();
 }
